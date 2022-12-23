36b24f1cbc4ce169bb22540d4c5bdab430934489
Lucas Prabant <lprabant@gmail.com>
2022-12-23 21:29:48 +0100

    changed pg to mysql

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -1,14 +1,12 @@
 module.exports = ({ env }) => ({
   connection: {
-    client: "postgres",
+    client: "mysql",
     connection: {
       host: env("DATABASE_HOST"),
       database: env("DATABASE_NAME"),
       user: env("DATABASE_USERNAME"),
       password: env("DATABASE_PASSWORD"),
-      ssl: {
-        rejectUnauthorized: env.bool("DATABASE_SSL_SELF", false),
-      },
+      ssl: env.bool("DATABASE_SSL", false),
     },
     debug: false,
   },
