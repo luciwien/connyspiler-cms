9d841cbdd6b32d3f27f566f946a2d0d3580be996
Lucas Prabant <lprabant@gmail.com>
2022-12-26 19:53:26 +0100

    added logs for environment variables

--- server.js
+++ server.js
@@ -1,4 +1,10 @@
 "use strict";
 const strapi = require("./node_modules/strapi/lib/index");
 
+console.log(env("NODE_ENV"));
+console.log(env("DATABASE_HOST"));
+console.log(env("DATABASE_NAME"));
+console.log(env("DATABASE_USERNAME"));
+console.log(env("DATABASE_PASSWORD"));
+
 strapi().start();
